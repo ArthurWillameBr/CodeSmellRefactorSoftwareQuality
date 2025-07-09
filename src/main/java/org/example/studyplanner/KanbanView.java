1f634e802eae417d9b4f5a9ba5ee6a955b66e00e
Arthur Willame <163607700+ArthurWillameBr@users.noreply.github.com>
2025-07-08 22:01:23 -0300

    fix: refactor code smell long method using Extract Method refactoring technique

--- src/main/java/org/example/studyplanner/KanbanView.java
+++ src/main/java/org/example/studyplanner/KanbanView.java
@@ -6,21 +6,18 @@ import java.util.List;
 import java.util.Map;
 
 public class KanbanView {
-    public enum State{
+    public enum State {
         TODO, DOING, DONE;
     }
 
-    HabitTracker habitTracker = null;
-    TodoTracker todoTracker = null;
-    Map<State, List<PlannerMaterial>> kanban = null;
+    private HabitTracker habitTracker;
+    private TodoTracker todoTracker;
+    private Map<State, List<PlannerMaterial>> kanban;
 
     public KanbanView(HabitTracker habitTracker, TodoTracker todoTracker) {
         this.habitTracker = habitTracker;
         this.todoTracker = todoTracker;
-        this.kanban = new HashMap<>();
-        this.kanban.put(State.TODO, new ArrayList<>());
-        this.kanban.put(State.DOING, new ArrayList<>());
-        this.kanban.put(State.DONE, new ArrayList<>());
+        this.kanban = initializeKanban();
     }
 
     public List<PlannerMaterial> getKanbanByState(State state) {
@@ -28,94 +25,63 @@ public class KanbanView {
     }
 
     public void addHabitToKanban(State state, Integer id) throws Exception {
-        try{
-            Habit toAdd = this.habitTracker.getHabitById(id);
-            if(toAdd == null){
-                throw new Exception("Habit not found with id: " + id);
-            }
-            kanban.get(state).add(toAdd);
-        } catch (Exception e){
-            throw new Exception(e.getMessage());
-        }
+        PlannerMaterial habit = getHabitOrThrow(id);
+        addToKanban(state, habit);
     }
 
     public void addToDoToKanban(State state, Integer id) throws Exception {
-        try{
-            ToDo toAdd = this.todoTracker.getToDoById(id);
-            if(toAdd == null){
-                throw new Exception("ToDo not found with id: " + id);
-            }
-            kanban.get(state).add(toAdd);
-        } catch (Exception e){
-            throw new Exception(e.getMessage());
-        }
+        PlannerMaterial todo = getToDoOrThrow(id);
+        addToKanban(state, todo);
     }
 
     public void removeHabitFromKanban(State state, Integer id) throws Exception {
-        try{
-            Habit toRemove = this.habitTracker.getHabitById(id);
-            if(toRemove == null) {
-                throw new Exception("No habit found with id: " + id);
-            }
-            kanban.get(state).remove(toRemove);
-        }catch (Exception e){
-            throw new Exception(e.getMessage());
-        }
+        PlannerMaterial habit = getHabitOrThrow(id);
+        removeFromKanban(state, habit);
     }
+
     public void removeToDoFromKanban(State state, Integer id) throws Exception {
-        try{
-            ToDo toRemove = this.todoTracker.getToDoById(id);
-            if(toRemove == null) {
-                throw new Exception("No todo found with id: " + id);
-            }
-            kanban.get(state).remove(toRemove);
-        } catch(Exception e){
-            throw new Exception(e.getMessage());
-        }
+        PlannerMaterial todo = getToDoOrThrow(id);
+        removeFromKanban(state, todo);
     }
 
     public String kanbanView() throws Exception {
-        try{
-
-            if(kanban.isEmpty()){
-                throw new Exception("No material found");
-            }
-            StringBuilder sb = new StringBuilder();
-            sb.append("[ Material ToDo: ");
-            sb.append(System.lineSeparator());
-
-            if(kanban.get(State.TODO).isEmpty()){
-                sb.append("No material found");
-            } else {
-                for(PlannerMaterial material : kanban.get(State.TODO)){
-                    sb.append(", ").append(material.toString());
-                }
-            }
-            sb.append(System.lineSeparator());
-            sb.append("Material in progress:");
-            sb.append(System.lineSeparator());
-            if(kanban.get(State.DOING).isEmpty()){
-                sb.append("No material found");
-            } else {
-                for(PlannerMaterial material : kanban.get(State.DOING)){
-                    sb.append(", ").append(material.toString());
-                }
-            }
-            sb.append(System.lineSeparator());
-            sb.append("Material completed:");
-            sb.append(System.lineSeparator());
-            if(kanban.get(State.DONE).isEmpty()){
-                sb.append("No material found");
-            } else {
-                for(PlannerMaterial material : kanban.get(State.DONE)){
-                    sb.append(", ").append(material.toString());
-                }
-            }
-            sb.append("]");
-            return sb.toString();
-        } catch (Exception e){
-            throw new Exception(e.getMessage());
+        if (kanban.isEmpty()) {
+            throw new Exception("No material found");
         }
+        return new KanbanRenderer(kanban).render();
     }
 
+    // ==== Métodos auxiliares extraídos ====
+
+    private Map<State, List<PlannerMaterial>> initializeKanban() {
+        Map<State, List<PlannerMaterial>> board = new HashMap<>();
+        for (State state : State.values()) {
+            board.put(state, new ArrayList<>());
+        }
+        return board;
+    }
+
+    private Habit getHabitOrThrow(Integer id) throws Exception {
+        Habit habit = habitTracker.getHabitById(id);
+        if (habit == null) {
+            throw new Exception("Habit not found with id: " + id);
+        }
+        return habit;
+    }
+
+    private ToDo getToDoOrThrow(Integer id) throws Exception {
+        ToDo todo = todoTracker.getToDoById(id);
+        if (todo == null) {
+            throw new Exception("ToDo not found with id: " + id);
+        }
+        return todo;
+    }
+
+    private void addToKanban(State state, PlannerMaterial item) {
+        kanban.get(state).add(item);
+    }
+
+    private void removeFromKanban(State state, PlannerMaterial item) {
+        kanban.get(state).remove(item);
+    }
 }
